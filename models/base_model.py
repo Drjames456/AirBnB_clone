43e1e659493605276d089481b7ca14d034cc51fe
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-02-10 05:17:17 -0800

    The BaseModel that defines all common attributes/methods for other classes that will be used for the Hbnb project

--- models/base_model.py
+++ models/base_model.py
@@ -13,17 +13,21 @@ class BaseModel:
     attributes/methods for other classes
     """
     def __init__(self):
+        """A constructor for the instance of the class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """A method that returns a string representation of an object"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """A method that updates the instance time of creation"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """A method that create an instance dictionary"""
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         self.__dict__["__class__"] = self.__class__.__name__

feecbe3b271cdd68a48d01c02bba681ab479b5c0
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-02-09 20:44:02 -0800

    The BaseModel that defines all common attributes/methods for other classes that will be used for the Hbnb project

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return f"[BaseModel] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
@@ -25,6 +25,6 @@ class BaseModel:
     def to_dict(self):
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
-        self.__dict__["__class__"] = 'BaseModel'
+        self.__dict__["__class__"] = self.__class__.__name__
         return self.__dict__
 

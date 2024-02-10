903511d40c6afcebfa73ff671fabda1450ee7d16
Osunba Adeola <osunbaadeola56@gmail.com>
2024-02-10 15:08:09 -0800

    AirBnB

--- dog.py
+++ dog.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-"""this module defines a class of Dog"""
+"""this module defines a function of Student data"""
 
 
-class Dog:
-    """this is an empty class"""
-    pass
+def Student_data():
+    """define a funtion that check the age of student"""
+    name = input("Please enter valid name: ")
+    lastname = input('please enter first  name: ')
+    username = name[:3] + lastname[3:]
+    age = input('please enter a age: ')
+    age = int(age)
+    if age > 30:
+        print('exceed  the age to take the test!')
+    elif age < 18:
+        print('Too young to take the test!')
+    else:
+        print('Proceed to take the test!')
+    print(username)
+
+
+Student_data()

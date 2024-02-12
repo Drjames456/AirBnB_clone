cf37ba4f59cbc7409c73dfd8e50f9705f34b55b4
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-02-12 06:53:33 -0800

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,7 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """A constructor for the instance of the class"""
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
